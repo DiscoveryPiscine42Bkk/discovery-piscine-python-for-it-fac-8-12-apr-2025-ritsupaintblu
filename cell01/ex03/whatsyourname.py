49dc35b40763ba980d4d36bdc6a0fff58eac113f
ritsupaintblu <aaomm9493@gmail.com>
2025-04-09 04:12:06 +0700

    fix cell01 ex03

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,10 +1,6 @@
 def main():
     name = input("Hey, what's your first name")
-    print(f"Hey, what's your first name? : {name}")
-
     last_name = input('And your last me?')
-    print(f'And your last me? : {last_name}')
-
 
     all = (f'Well, pleased to meet you, {name} {last_name}')
     print(all)
