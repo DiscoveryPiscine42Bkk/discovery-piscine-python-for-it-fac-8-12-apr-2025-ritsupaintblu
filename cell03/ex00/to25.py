bbab126877c3570e88c9e7c76d6b59cfe30a7023
ritsupaintblu <aaomm9493@gmail.com>
2025-04-12 19:44:55 +0700

    fix cell03, add cell04 cell05

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,7 +1,7 @@
 def main():
     n = int(input("Enter a number less than 25 "))
-    if n < 25:
-        while n <=25:
+    if n <= 25:
+        while n <= 25:
             print(f"Inside the loop, my variable is {n}")
             n += 1
     else:
---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,5 @@
+def main():
+    n = float(input("Give me a number : "))
+    print(round(n + 0.5))
+
+main()
